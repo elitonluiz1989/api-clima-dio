f1155db4aaaba402b0fed69dd3637df27b6544e4
elitonluiz1989 <elitonluiz1989@gmail.com>
2025-01-30 20:41:11 -0400

    Refatorando o código para separar melhor os trechos da aplicação

---
+++ ApiClimaDio/WeatherForecastEndpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiClimaDio;
+
+public static class WeatherForecastEndpoints
+{
+    public static void AddWeatherForecastEndpoint(WebApplication app)
+    {
+        app.MapGet("/weatherforecast", WeatherForecastService.GetWeatherForecast).WithName("GetWeatherForecast");
+    }
+}
---
+++ ApiClimaDio/WeatherForecastService.cs
@@ -0,0 +1,32 @@
+﻿namespace ApiClimaDio;
+
+public static class WeatherForecastService
+{
+    private static string[] _summaries = [
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool", "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching"
+    ];
+
+    public static WeatherForecast[] GetWeatherForecast()
+    {
+        WeatherForecast[] forecast = Enumerable
+            .Range(1, 5)
+            .Select(index =>
+                new WeatherForecast (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    _summaries[Random.Shared.Next(_summaries.Length)]
+                )
+            )
+            .ToArray();
+
+        return forecast;
+    }
+}
